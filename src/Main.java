0ad26970d649d7e114cd0e87f1b372ad8d9edd51
LeszekS <leszekstepien@o2.pl>
2019-01-17 10:51:21 +0100

    Version 4 Końcowa z rabatami

--- src/Main.java
+++ src/Main.java
@@ -10,21 +10,21 @@ public class Main {
         Product product2 = new Product("Romet","Dzieciecy", 700, category);
 
         opisProd = "typ Scigacz";
-        Category motors = new Category("MotorRowery",opisProd);
+        Category motors = new Category("Motor",opisProd);
         Product product3 = new Product("Honda","jednoosobowy", 25_000,motors);
 
         Product product4 = new Product("Honda","jednoosobowy", 25_000);
 
+
+        Product product5 = new Product("Honda","Rocznik 2017", 15_000,motors);
+        SpecialOffer specialOffer = new SpecialOffer(product5,"Wyprzedaż rocznika","2019-02-01","2019-02-28",0.2);
 
 
         System.out.println(product1.opis());
         System.out.println(product2.opis());
         System.out.println(product3.opis());
         System.out.println(product4.opis1());
-
-//        System.out.println(product.category.nameCategory);
-
-//        System.out.println(product.category.descriptionCategory);
+        System.out.println(specialOffer.opis());
 
     }
 
--- src/SpecialOffer.java
+++ src/SpecialOffer.java
@@ -1,11 +1,38 @@
 
 import java.util.Date;
 
+import static jdk.nashorn.internal.objects.NativeMath.round;
+
 public class SpecialOffer {
     Product product;
     String ofertaPromocyjna;
-    Date poczatekPromocji;
-    Date konecPromocji;
+    String poczatekPromocji;
+    String konecPromocji;
     double rabat;
+
+
+    public SpecialOffer(Product product, String ofertaPromocyjna, String poczatekPromocji, String konecPromocji, double rabat) {
+        this.product = product;
+        this.ofertaPromocyjna = ofertaPromocyjna;
+        this.poczatekPromocji = poczatekPromocji;
+        this.konecPromocji = konecPromocji;
+        this.rabat = rabat;
+        this.product.price = round(2, product.price * (1 - rabat));
+    }
+
+    String opis() {
+        String opisProduktu = "Nazwa - " + product.name +
+                ",\nOpis        - " + product.descryption +
+                ",\nRabat       - " + rabat * 100 + "%" +
+                ",\nCena po rab.- " + product.price + "zł" +
+                ",\nTyp         - " + product.category.nameCategory +
+                ",\nKat. Opis   - " + product.category.descriptionCategory +
+                ",\nPoczątek promocji - " + poczatekPromocji +
+                ",\nKoniec   promocji - " + konecPromocji;
+
+
+        return opisProduktu;
+    }
+
 
 }
